76ebf621b1cc7f60303bc46c81451d9e977d1e04
Arnaud JEZEQUEL <neckaros@gmail.com>
2024-03-20 10:41:06 +0100

    0.5.0 add Request status

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use rs_plugin_common_interfaces::PluginCredential;
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumString;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 #[serde(rename_all = "snake_case")] 
@@ -9,13 +10,35 @@ pub struct RsRequest {
     pub size: Option<u64>,
     pub filename: Option<String>,
     #[serde(default)]
-    pub require_add: bool,
-    #[serde(default)]
-    pub intermediate: bool,
-    pub cookies: Option<String>,
+    pub status: RsRequestStatus,
     pub headers: Option<Vec<(String, String)>>,
-    pub files: Option<Vec<RsRequestFiles>>
+    /// If must choose between multiple files. Recall plugin with a `selected_file` containing one of the name in this list to get link
+    pub files: Option<Vec<RsRequestFiles>>,
+    /// one of the `files` selected for download
+    pub selected_file: Option<String>
 }
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, strum_macros::Display,EnumString, Default)]
+#[serde(rename_all = "camelCase")] 
+#[strum(serialize_all = "camelCase")]
+pub enum RsRequestStatus {
+    /// No plugin yet processed this request
+    #[default]
+	Unprocessed,
+    /// Link can be processed but first need to be added to the service and downloaded
+    ///   -First call this plugin again with `add` method
+    ///   -Check status and once ready call `process` again
+    RequireAdd,
+    /// Other plugin can process it
+    Intermediate,
+    /// Multiple files found, current plugin need to be recalled with a `selected_file``
+    NeedFileSelection,
+    /// `url` is ready but should be proxied by the server as it contains sensitive informations (like token)
+    FinalPrivate,
+    /// `url` is ready and can be directly sent to _any_ user directly (using redirect)
+    FinalPublic
+}
+
 
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
